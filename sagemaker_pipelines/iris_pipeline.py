60fced50f851f9173a452057bedb1cc32c9add54
AlbertStats19 <ycaperam@unal.edu.co>
2025-07-22 14:43:42 -0500

    update size compute train

--- sagemaker_pipelines/iris_pipeline.py
+++ sagemaker_pipelines/iris_pipeline.py
@@ -16,7 +16,9 @@ from sagemaker.workflow.parameters import (
 )
 
 # parameter_custom_lib = "ml.m5.large"
-parameter_custom_lib = "ml.t3.medium"
+# parameter_custom_lib = "ml.t3.medium"
+# validar métrica
+parameter_custom_lib ="ml.m5.xlarge"
 
 # La función get_sagemaker_pipeline ahora aceptará los parámetros
 # que serán inyectados por CodeBuild.
